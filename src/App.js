18603332641b6bb530fc675e8e4ee40a76eb29ef
Ivan Reis <ivreis96@gmail.com>
2024-07-12 08:23:01 -0300

    Update App.js
    Pequeno ajuste

--- src/App.js
+++ src/App.js
@@ -1,7 +1,3 @@
-// import React from 'react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-// import HomePage from './HomePage'; // Sua página principal
-// import TestPage from './TestPage'; // Sua nova página de testes
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import HomePage from './HomePage';
